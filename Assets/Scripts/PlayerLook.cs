39cc9d6b62d17449011945122c251342b2e074e5
Alan Jantz <alan_f_jantz@hotmail.com>
2021-08-23 22:09:38 -0300

    Controlar campo de visão com controle

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSensitivity = 150f;
+
+    public Transform playerBody;
+
+    private float horizontalRotation;
+    private float xRotation = 0f;
+    private float verticalRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void OnHorizontalRotation(InputValue input)
+    {
+        var value = input.Get<float>();
+
+        Debug.Log($"OnHorizontalRotation: {value}");
+
+        horizontalRotation = value;
+    }
+
+    void OnVerticalRotation(InputValue input)
+    {
+        var value = input.Get<float>();
+
+        verticalRotation = value;
+    }
+
+    void Update()
+    {
+        float xAxis = horizontalRotation * mouseSensitivity * Time.deltaTime;
+        float yAxis = verticalRotation * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= yAxis;
+
+        if (xRotation <= -360f)
+            xRotation = 0f;
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * xAxis);
+    }
+}
